880d49ee363cccac45f02e11912954f71ab06f0f
lilyjetton <lmjetton@mavs.coloradomesa.edu>
2025-09-04 14:41:07 -0600

    Add files via upload

---
+++ Assigment 1.py
@@ -0,0 +1,19 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> word1 = "all"
+>>> word2 = "work"
+>>> Word3 = "and"
+>>> Word4 = "no"
+>>> word5 = "play"
+>>> word6 = "makes"
+>>> word7 = "jack"
+>>> word8 = "a"
+>>> Word9 = "dule"
+>>> word10 = "boy"
+>>> pint(word1,word2,Word3,Word4,word5,word6,word7,word8,Word9,word10)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    pint(word1,word2,Word3,Word4,word5,word6,word7,word8,Word9,word10)
+NameError: name 'pint' is not defined. Did you mean: 'print'?
+>>> print(word1,word2,Word3,Word4,word5,word6,word7,word8,Word9,word10)
+all work and no play makes jack a dule boy
