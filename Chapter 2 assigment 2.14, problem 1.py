761432da12bca43f25db485c01f06b5989b80f19
lilyjetton <lmjetton@mavs.coloradomesa.edu>
2025-09-09 13:42:40 -0600

    Add files via upload

---
+++ Chapter 2 assigment 2.14, problem 1.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Chapter 2 assigment 2.14, problem 1
+# Purpose:
+#
+# Author:      Lilyj
+#
+# Created:     09/09/2025
+# Copyright:   (c) Lilyj 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
