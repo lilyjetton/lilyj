17fe84a96d3591ff1c76b825bbe825a274b9fa54
lilyjetton <lmjetton@mavs.coloradomesa.edu>
2025-09-09 15:25:34 -0600

    Add files via upload

---
+++ Chapter 2, assigment 2.14 problem 1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Lilyj
+#
+# Created:     09/09/2025
+# Copyright:   (c) Lilyj 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+word1= "all"
+word2= "work"
+word3= "and"
+word4= "no"
+word5= "play"
+word6= "makes"
+word7= "jack"
+word8= "a"
+word9= "dull"
+word10= "boy"
+print(word1,word2,word3,word4,word5,word6,word7,word8,word9,word10)
+all work and no play makes jack a dull boy
