95daa554f3ebb1f142511e3990ae96959941c5dd
lilyjetton <lmjetton@mavs.coloradomesa.edu>
2025-09-11 13:13:28 -0600

    Add files via upload

---
+++ Section 3.8 question 11.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        Section 3.8 question 11
+# Purpose:
+#
+# Author:      Lilyj
+#
+# Created:     10/09/2025
+# Copyright:   (c) Lilyj 2025
+# Licence:     <your licence>
+from turtle import *
+forward (50)
+right (50)
+forward (50)
+right (50)
+forward (50)
+right (50)
+forward (50)
+right (50)
+forward (50)
+right (50)
+forward (50)
+right (50)
+forward (50)
