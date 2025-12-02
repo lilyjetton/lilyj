101ca4f61f09cf1f1d6ad719a177f24e8990d6c4
lilyjetton <lmjetton@mavs.coloradomesa.edu>
2025-12-02 14:05:56 -0700

    Add files via upload

--- final/final.py
+++ final/final.py
@@ -2,7 +2,8 @@ def readboard(filename):
     # Open the file and read all lines without newline characters
     with open(filename, "r") as f:
         lines = f.read().splitlines()
-        board = [list(row) for row in lines]  # Convert each string to a list
+
+    board = [list(row) for row in lines]  # Convert each string to a list
     return board
 def start_game():
     print("Starting the game...")
@@ -43,6 +44,60 @@ def menu():
             break
         else:
             print("Invalid choice, please try again.")
+    
+    
+def printboard(board):
+    # Print each row with spaces between numbers
+    for rows in board:
+        print(" ".join(str(num) for num in rows))
+
+def playermakemove(board):
+    print("enter point(x,y)")
+    x = int(input("x "))
+    y = int(input("y "))
+    if board[x][y] == "1": # checking if its a hit
+        board[x][y] = "+"
+    if board[x][y] == "0": # checking if theres a miss 
+        board[x][y] = "x"
+
+def main():
+    keepRunning = True
+
+    while keepRunning:
+        choice = menu()
+
+        if choice == "1":
+            playerBoard = readboard("battleship.txt")
+            print("\n--- GAME START ---")
+            printboard(playerBoard)
+
+            # Game loop
+            while True:
+                playermakemove(playerBoard) # shows updated board each turn
+
+        elif choice == "2":
+            print("Instructions")
+            keepRunning = False
+
+        else:
+            print("Invalid choice. Try again.")
+
+    # Read the game board from the text file
+    playerBoard = readboard("battleship.txt")
+# board = [ [0, 0, 0],
+#           [0, 0, 0],
+#           [0, 0, 0],
+#           [0, 0, 0] ]
+#board[1][2] = 1
+    # Print the board
+    keepRunning = True
+    while(keepRunning == True):
+        printboard(playerBoard)
+        playermakemove(playerBoard)
+    
+
+if __name__ == "__main__":
+        main()
 
         
 
